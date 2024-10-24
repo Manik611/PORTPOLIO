a3113a277f2f9d37a206a05a15a8208afe6ff5ce
Manik611 <manikmondal202h@gmail.com>
2024-10-24 11:50:26 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,11 @@
+// Optional JavaScript for interaction and smooth scrolling
+
+// Smooth scrolling when clicking on the navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
